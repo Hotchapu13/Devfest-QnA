e7a91405ff0df25b72a5aaaa234a0fc389e6e18f
Bryant <nuwebryant@gmail.com>
2025-10-27 13:30:31 +0300

    Add admin site tracking of intial models

--- qna_app/admin.py
+++ qna_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import User, Session, Question, Answer, SpeakerSession
 
-# Register your models here.
+admin.site.register([User, Session, Question, Answer, SpeakerSession])
+
